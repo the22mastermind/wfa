a7e282e9033c29b612afe0d82a36a8ade8ce90a9
the22mastermind <bertrandmasabo22@gmail.com>
2020-02-22 23:59:18 +0200

    Add user signup component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/layout/Header';
 import LandingPage from './components/pages/LandingPage';
+import SignUp from './components/pages/SignUp';
 
 const App = () => {
   return (
@@ -10,6 +11,9 @@ const App = () => {
       <React.Fragment>
         <Header />
         <Switch>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
           <Route path="/">
             <LandingPage />
           </Route>
